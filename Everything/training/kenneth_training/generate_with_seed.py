5dac246b42e3f43df4754fec6475fde6c0b9cc97
Kenneth Gomez <kenneth@dhcp-10-105-120-42.wireless.northwestern.private>
2018-02-19 19:48:19 -0600

    added front end stuff

--- Everything/training/kenneth_training/generate_with_seed.py
+++ Everything/training/kenneth_training/generate_with_seed.py
@@ -22,26 +22,25 @@ def sample(preds, temperature=1.0):
 # Arguments: (model, name of pickle file, length of sample)
 def generate_with_seed(model, picklename, length):
 
-	# LOAD THE PICKLE
-	with open(picklename, 'rb') as handle:
-		[ix_to_char, char_to_ix] = pickle.load(handle)
+	# LOAD THE PICKL
 
-	VOCAB_SIZE = len(ix_to_char)
-	seed = input("Enter seed text: ")
-	print("\n")
+    with open("word_mappings.p", 'rb') as handle:
+        [ix_to_char, char_to_ix] = pickle.load(handle)
 
-	ix = [char_to_ix[seed[-1]]]
-	y_char = [ix_to_char[ix[-1]]]
-	X = np.zeros((1, length, VOCAB_SIZE))
-	for t, char in enumerate(seed):
-		X[0, t, :][char_to_ix[char]] = 1
+    VOCAB_SIZE = len(ix_to_char)
+    seed = input("enter seed text: ")
+    print("\n")
 
-	for i in range(length):
-		X[0, i, :][ix[-1]] = 1
-		print(ix_to_char[ix[-1]], end="")
-		ix = np.argmax(model.predict(X[:, :i+1, :])[0], 1)
-		y_char.append(ix_to_char[ix[-1]])
-	return ('').join(y_char)
+    ix = [np.random.randint(VOCAB_SIZE)]
+    y_char = [ix_to_char[ix[-1]]]
+    X = np.zeros((1, length, VOCAB_SIZE))
+
+    for i in range(length):
+        X[0,i,:][ix[-1]] = 1
+        print(ix_to_char[ix[-1]], end=" ")
+        ix = np.argmax(model.predict(X[:, :i+1, :])[0], 1)
+        y_char.append(ix_to_char[ix[-1]])
+    return ('').join(y_char)
 
 	# ix = [np.random.randint(VOCAB_SIZE)]
 	# y_char = [ix_to_char[ix[-1]]]
